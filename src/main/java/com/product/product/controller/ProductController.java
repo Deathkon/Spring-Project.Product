b46fadbc7b8b41567fb14236878c571964930373
Deathkon <74615179+Deathkon@users.noreply.github.com>
2021-12-24 12:21:25 +0200

    commit

--- src/main/java/com/product/product/controller/ProductController.java
+++ src/main/java/com/product/product/controller/ProductController.java
@@ -3,13 +3,18 @@ package com.product.product.controller;
 import com.product.product.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
-
+    @GetMapping("/")
+    public String viewHome(Model model) {
+        model.addAttribute("listProduct", productRepository.findAll());
+        return "index";
+    }
     
 }
